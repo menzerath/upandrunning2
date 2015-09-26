08c8c23f265f14784dd45b0f850d6f6081e8b980
Marvin Menzerath <marvin.menzerath@gmx.de>
2015-09-26 22:36:24 +0200

    Added Websites- and Website-Detail-API

--- routes/api.go
+++ routes/api.go
@@ -2,27 +2,102 @@ package routes
 
 import (
 	"encoding/json"
+	"github.com/MarvinMenzerath/UpAndRunning2/lib"
 	"github.com/julienschmidt/httprouter"
+	"github.com/op/go-logging"
 	"net/http"
+	"strconv"
 )
 
 func ApiIndex(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	responseJson := BasicResponse{true, "Welcome to UpAndRunning's API!"}
-
-	responseBytes, err := json.Marshal(responseJson)
-	if err != nil {
-		http.Error(w, "Error 500: Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
+	responseBytes, _ := json.Marshal(BasicResponse{true, "Welcome to UpAndRunning's API!"})
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(responseBytes)
 }
 
 func ApiStatus(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Query the Database
+	db := lib.GetDatabase()
+	stmt, err := db.Prepare("SELECT id, name, protocol, url, status, time, lastFailStatus, lastFailTime, ups, downs, totalChecks, avgAvail FROM website WHERE url = ? AND enabled = 1;")
+	if err != nil {
+		logging.MustGetLogger("logger").Error("Unable to fetch Websites: ", err)
+		SendJsonMessage(w, http.StatusInternalServerError, "Unable to process your Request.")
+		return
+	}
 
+	var (
+		id             int
+		name           string
+		protocol       string
+		url            string
+		status         string
+		time           string
+		lastFailStatus string
+		lastFailTime   string
+		ups            int
+		downs          int
+		totalChecks    int
+		avgAvail       float64
+	)
+
+	err = stmt.QueryRow(ps.ByName("url")).Scan(&id, &name, &protocol, &url, &status, &time, &lastFailStatus, &lastFailTime, &ups, &downs, &totalChecks, &avgAvail)
+	if err != nil {
+		logging.MustGetLogger("logger").Error("Unable to fetch Websites: ", err)
+		SendJsonMessage(w, http.StatusInternalServerError, "Unable to process your Request.")
+		return
+	}
+
+	// Build Response
+	responseJson := DetailedWebsiteResponse{true, WebsiteData{id, name, protocol + "://" + url}, WebsiteAvailability{ups, downs, totalChecks, strconv.FormatFloat(avgAvail, 'f', 2, 64) + "%"}, WebsiteCheckResult{status, time}, WebsiteCheckResult{lastFailStatus, lastFailTime}}
+
+	// Send Response
+	responseBytes, err := json.Marshal(responseJson)
+	if err != nil {
+		SendJsonMessage(w, http.StatusInternalServerError, "Unable to process your Request.")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseBytes)
 }
 
 func ApiWebsites(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Query the Database
+	db := lib.GetDatabase()
+	rows, err := db.Query("SELECT name, protocol, url, status FROM website WHERE enabled = 1 AND visible = 1;")
+	if err != nil {
+		logging.MustGetLogger("logger").Error("Unable to fetch Websites: ", err)
+		SendJsonMessage(w, http.StatusInternalServerError, "Unable to process your Request.")
+		return
+	}
+	defer rows.Close()
 
+	// Add every Website
+	websites := []BasicWebsite{}
+	for rows.Next() {
+		var row BasicWebsite
+		err = rows.Scan(&row.Name, &row.Protocol, &row.Url, &row.Status)
+		if err != nil {
+			logging.MustGetLogger("logger").Error("Unable to read Website-Row: ", err)
+			SendJsonMessage(w, http.StatusInternalServerError, "Unable to process your Request.")
+			return
+		}
+		websites = append(websites, row)
+	}
+
+	// Check for Errors
+	err = rows.Err()
+	if err != nil {
+		logging.MustGetLogger("logger").Error("Unable to read Website-Rows: ", err)
+		SendJsonMessage(w, http.StatusInternalServerError, "Unable to process your Request.")
+		return
+	}
+
+	// Send Response
+	responseBytes, err := json.Marshal(WebsiteResponse{true, websites})
+	if err != nil {
+		SendJsonMessage(w, http.StatusInternalServerError, "Unable to process your Request.")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseBytes)
 }
--- routes/response.go
+++ routes/response.go
@@ -1,10 +1,57 @@
 package routes
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Basic Response
 type BasicResponse struct {
 	Success bool   `json:"requestSuccess"`
 	Message string `json:"message"`
 }
 
+// Website Response
+type WebsiteResponse struct {
+	Success  bool           `json:"requestSuccess"`
+	Websites []BasicWebsite `json:"websites"`
+}
+
+type BasicWebsite struct {
+	Name     string `json:"name"`
+	Protocol string `json:"protocol"`
+	Url      string `json:"url"`
+	Status   string `json:"status"`
+}
+
+// Detailed Website-Status Response
+type DetailedWebsiteResponse struct {
+	Success               bool                `json:"requestSuccess"`
+	WebsiteData           WebsiteData         `json:"websiteData"`
+	Availability          WebsiteAvailability `json:"availability"`
+	LastCheckResult       WebsiteCheckResult  `json:"lastCheckResult"`
+	LastFailedCheckResult WebsiteCheckResult  `json:"lastFailedCheckResult"`
+}
+
+type WebsiteData struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Url  string `json:"url"`
+}
+
+type WebsiteAvailability struct {
+	Ups     int    `json:"ups"`
+	Downs   int    `json:"downs"`
+	Total   int    `json:"total"`
+	Average string `json:"average"`
+}
+
+type WebsiteCheckResult struct {
+	Status string `json:"status"`
+	Time   string `json:"time"`
+}
+
+// Site-Data
 type SiteData struct {
 	Title string
 }
@@ -16,4 +63,12 @@ type AdminSiteData struct {
 	AppVersion string
 	GoVersion  string
 	GoArch     string
+}
+
+// Functions
+func SendJsonMessage(w http.ResponseWriter, code int, message string) {
+	responseBytes, _ := json.Marshal(BasicResponse{false, message})
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(responseBytes)
 }
