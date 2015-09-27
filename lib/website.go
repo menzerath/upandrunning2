145c7eaaea783cc9aa7dfbac10edebee57f34fbf
Marvin Menzerath <marvin.menzerath@gmx.de>
2015-09-27 18:54:42 +0200

    Added `calcAvgAvailability()` to Website

--- lib/website.go
+++ lib/website.go
@@ -1,8 +1,10 @@
 package lib
+
 import (
 	"github.com/mitsuse/pushbullet-go"
 	"github.com/mitsuse/pushbullet-go/requests"
 	"github.com/op/go-logging"
+	"strconv"
 )
 
 type Website struct {
@@ -16,7 +18,35 @@ func (w *Website) RunCheck() {
 }
 
 func (w *Website) calcAvgAvailability() {
+	// Query the Database
+	db := GetDatabase()
+	stmt, err := db.Prepare("SELECT ((SELECT ups FROM website WHERE id = ?) / (SELECT totalChecks FROM website WHERE id = ?))*100 AS avg")
+	if err != nil {
+		logging.MustGetLogger("logger").Error("Unable to calculate Website-Availability: ", err)
+		return
+	}
 
+	var avg float64
+	err = stmt.QueryRow(w.Id, w.Id).Scan(&avg)
+	if err != nil {
+		logging.MustGetLogger("logger").Error("Unable to calculate Website-Availability: ", err)
+		return
+	}
+
+	strconv.FormatFloat(avg, 'f', 2, 64)
+
+	// Save the new value
+	stmt, err = db.Prepare("UPDATE website SET avgAvail = ? WHERE id = ?;")
+	if err != nil {
+		logging.MustGetLogger("logger").Error("Unable to calculate Website-Availability: ", err)
+		return
+	}
+
+	_, err = stmt.Exec(avg, w.Id)
+	if err != nil {
+		logging.MustGetLogger("logger").Error("Unable to save Website-Availability: ", err)
+		return
+	}
 }
 
 func sendPush(name string, url string, newStatus string, oldStatus string) {
@@ -32,7 +62,7 @@ func sendPush(name string, url string, newStatus string, oldStatus string) {
 	push.Title = GetConfiguration().Dynamic.Title + " - Status Change"
 	push.Body = name + " (" + url + ") went from \"" + oldStatus + "\" to \"" + newStatus + "\"."
 
-	_, err := pb.PostPushesNote(push);
+	_, err := pb.PostPushesNote(push)
 	if err != nil {
 		logging.MustGetLogger("logger").Error("Unable to send Push: ", err)
 	}
