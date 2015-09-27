a5d6baee79027b2182d9ce3ab1a3a167e78f2c70
Marvin Menzerath <marvin.menzerath@gmx.de>
2015-09-27 18:42:20 +0200

    Added Pushbullet-API

--- lib/website.go
+++ lib/website.go
@@ -1,4 +1,9 @@
 package lib
+import (
+	"github.com/mitsuse/pushbullet-go"
+	"github.com/mitsuse/pushbullet-go/requests"
+	"github.com/op/go-logging"
+)
 
 type Website struct {
 	Id       int
@@ -7,9 +12,28 @@ type Website struct {
 }
 
 func (w *Website) RunCheck() {
-
+	w.calcAvgAvailability()
 }
 
 func (w *Website) calcAvgAvailability() {
 
+}
+
+func sendPush(name string, url string, newStatus string, oldStatus string) {
+	if GetConfiguration().Dynamic.PushbulletKey == "" {
+		return
+	}
+
+	logging.MustGetLogger("logger").Debug("Sending Push about \"" + url + "\"...")
+
+	pb := pushbullet.New(GetConfiguration().Dynamic.PushbulletKey)
+
+	push := requests.NewNote()
+	push.Title = GetConfiguration().Dynamic.Title + " - Status Change"
+	push.Body = name + " (" + url + ") went from \"" + oldStatus + "\" to \"" + newStatus + "\"."
+
+	_, err := pb.PostPushesNote(push);
+	if err != nil {
+		logging.MustGetLogger("logger").Error("Unable to send Push: ", err)
+	}
 }
