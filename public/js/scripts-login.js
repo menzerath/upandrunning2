a9ea7f6d160ba9e37f2f554e474cb58b33465a45
Marvin Menzerath <github@marvin-menzerath.de>
2016-02-14 16:56:12 +0100

    Added SweetAlert to Login

--- public/js/scripts-login.js
+++ public/js/scripts-login.js
@@ -11,26 +11,19 @@ $(document).ready(function() {
 function login() {
 	var password = $('#input-password').val();
 
-	$('.bottom-right').notify({
-		type: 'warning',
-		message: {text: "Processing..."},
-		fadeOut: {enabled: true, delay: 3000}
-	}).show();
-
 	if (password.trim()) {
 		$.ajax({
 			url: "/api/v1/auth/login",
 			type: "POST",
 			data: {"password": password},
 			success: function() {
-				window.location.replace("/admin");
+				swal("Success!", "Logging you in...", "success");
+				setTimeout(function() {
+					window.location.replace("/admin");
+				}, 1000);
 			},
 			error: function(error) {
-				$('.bottom-right').notify({
-					type: 'danger',
-					message: {text: JSON.parse(error.responseText).message},
-					fadeOut: {enabled: true, delay: 3000}
-				}).show();
+				swal("Oops!", JSON.parse(error.responseText).message, "error");
 			}
 		});
 	}
