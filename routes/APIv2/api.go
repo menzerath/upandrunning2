c087a5ebdd4d94ce4de05ca271c08da1c4d95944
Marvin Menzerath <github@marvin-menzerath.de>
2016-08-02 17:26:10 +0200

    Removed unneeded function

--- routes/APIv2/api.go
+++ routes/APIv2/api.go
@@ -6,11 +6,6 @@ import (
 )
 
 // Sends a simple welcome-message to the user.
-func ApiIndex(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	SendJsonMessage(w, http.StatusOK, true, "Welcome to UpAndRunning2's API!")
-}
-
-// Sends a simple welcome-message to the user.
 func ApiIndexVersion(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	SendJsonMessage(w, http.StatusOK, true, "Welcome to UpAndRunning2's API v2!")
 }
