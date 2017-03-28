a6c2a43aae1e575688c2d74b12d4b40ad9e45d62
Marvin Menzerath <github@marvin-menzerath.de>
2017-03-28 19:55:30 +0200

    Run Docker container as custom user

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /app/upandrunning2/
 COPY . /app/upandrunning2/
 RUN chmod +x ./docker/build.sh && sync && ./docker/build.sh
 
+USER uar2
 EXPOSE 8080
 VOLUME /app/upandrunning2/config/
 ENTRYPOINT ["./UpAndRunning2"]
--- docker/build.sh
+++ docker/build.sh
@@ -5,19 +5,26 @@
 set -x
 set -e
 
+# set path
 export GOPATH=/tmp/go
 export PATH=${PATH}:${GOPATH}/bin
 
-apk -U --no-progress add git go build-base
+# install dependencies
+apk -U --no-progress add git go build-base ca-certificates
 
+# build application
 mkdir -p ${GOPATH}/src/github.com/MarvinMenzerath/
 ln -s /app/upandrunning2/ ${GOPATH}/src/github.com/MarvinMenzerath/UpAndRunning2
 cd ${GOPATH}/src/github.com/MarvinMenzerath/UpAndRunning2
 go get
 go build
 
+# remove sources and unnecessary packages
 rm -rf $GOPATH
 apk --no-progress del git go build-base
 
-# keep this one to enable telegram notifications
-apk -U --no-progress add ca-certificates
+# create custom user and group
+addgroup -g 1777 uar2 && adduser -h /app/upandrunning2/ -H -D -G uar2 -u 1777 uar2
+
+# set rights
+chown -R uar2:uar2 /app/upandrunning2/
